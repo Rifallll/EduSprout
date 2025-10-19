0712e4a6e46498fbd7630188cd3625429e7d1391
RIFAL AZHAR PERMANA <permanarifal269@gmail.com>
2025-10-19 21:33:06 +0700

    [dyad] Menghapus aplikasi Flask, mengintegrasikan skrip scraping Python yang lebih lengkap, dan menghapus halaman detail beasiswa internal. - wrote 2 file(s), renamed 1 file(s), deleted 3 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ import PricingPage from "./pages/PricingPage";
 import BasicPlanConfirmationPage from "./pages/BasicPlanConfirmationPage";
 import ProPlanPaymentPage from "./pages/ProPlanPaymentPage";
 import EnterpriseContactPage from "./pages/EnterpriseContactPage";
-import ScholarshipDetailPage from "./pages/ScholarshipDetailPage"; // Import the new ScholarshipDetailPage
+// import ScholarshipDetailPage from "./pages/ScholarshipDetailPage"; // Import the new ScholarshipDetailPage
 
 const queryClient = new QueryClient();
 
@@ -47,7 +47,7 @@ const App = () => (
             <Route index element={<Index />} />
             <Route path="events" element={<Events />} />
             <Route path="scholarships" element={<Scholarships />} />
-            <Route path="scholarships/:scholarshipId" element={<ScholarshipDetailPage />} /> {/* New route for scholarship details */}
+            {/* <Route path="scholarships/:scholarshipId" element={<ScholarshipDetailPage />} /> */} {/* Removed route for scholarship details */}
             <Route path="jobs" element={<Jobs />} />
             <Route path="jobs/:jobId" element={<JobDetailPage />} />
             <Route path="news-and-tips" element={<NewsAndTips />} />
